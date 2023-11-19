da559190c4566888c72a4d786ca7d5edaa33008f
rick-ssa <rick_ssa@hotmail.com>
2023-11-19 12:04:51 -0300

    created keybord arrow enum and directions type

---
+++ src/enums/KeyboardArrows.tsx
@@ -0,0 +1,6 @@
+export enum keyboardArrows {
+    ARROW_DOWN = 'ArrowDown',
+    ARROW_UP = 'ArrowUp',
+    ARROW_LEFT = 'ArrowLeft',
+    ARROW_RIGHT = 'ArrowRight',
+}
---
+++ src/models/TDirections.ts
@@ -0,0 +1,13 @@
+import { keyboardArrows } from "../enums/KeyboardArrows"
+
+type TVerticalDirection = {
+    direction: 'vertical',
+    arrow: keyboardArrows.ARROW_UP | keyboardArrows.ARROW_DOWN
+}
+
+type THorizontalDirection = {
+    direction: 'horizontal',
+    arrow: keyboardArrows.ARROW_RIGHT | keyboardArrows.ARROW_LEFT
+}
+
+export type TDirections = TVerticalDirection | THorizontalDirection
