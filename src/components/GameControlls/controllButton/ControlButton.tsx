e207312d500c041add3b1b1753a145750c9d865a
rick-ssa <rick_ssa@hotmail.com>
2023-12-14 20:12:34 -0300

    created controll component. close #32

---
+++ src/components/GameControlls/controllButton/ControlButton.tsx
@@ -0,0 +1,19 @@
+import { IconType } from "react-icons"
+import "./styles.css"
+
+export interface IControlButton {
+    icon: ()=>IconType
+    disabled?: boolean
+    onClick: () => {}
+}
+
+const ControllButton = ({icon, disabled = false, onClick}: IControlButton) => {
+    const Icon = icon()
+    return (
+        <button className="controll_button" disabled={disabled} onClick = {onClick}>
+            <Icon />
+        </button>
+    )
+}
+
+export default ControllButton
